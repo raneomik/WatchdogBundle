16ae8e11210bc4519f224c2c156ee4d2f3805efa
Marek <m.necesany@choosit.com>
2021-03-03 13:08:31 +0100

    adding coveralls

--- tests/Integration/TestCase.php
+++ tests/Integration/TestCase.php
@@ -4,18 +4,27 @@ namespace Raneomik\WatchdogBundle\Test\Integration;
 
 use Raneomik\WatchdogBundle\Test\Integration\Stubs\Kernel;
 use Symfony\Bundle\FrameworkBundle\Test\KernelTestCase;
+use Symfony\Component\Filesystem\Filesystem;
 
 abstract class TestCase extends KernelTestCase
 {
-    use ForwardCompatTestCaseTrait;
+    protected function setUp(): void
+    {
+        $fs = new Filesystem();
+        $fs->remove(\sys_get_temp_dir().'/WatchdogBundle/');
+    }
+
+    protected function tearDown(): void
+    {
+        static::ensureKernelShutdown();
+        static::$kernel = null;
+    }
 
     /**
      * {@inheritdoc}
      */
     protected static function createKernel(array $options = [])
     {
-        require_once __DIR__.'/Stubs/Kernel.php';
-
         return new Kernel('test', true, isset($options['config']) ? $options['config'] : 'base');
     }
 }
